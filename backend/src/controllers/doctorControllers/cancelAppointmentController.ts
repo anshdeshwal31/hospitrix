d15eb54699d665bbb53e3176dad34c9a3dd28382
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-02-18 21:23:38 +0530

    made doctor to cancel appointment

--- backend/src/controllers/doctorControllers/cancelAppointmentController.ts
+++ backend/src/controllers/doctorControllers/cancelAppointmentController.ts
@@ -1,7 +1,8 @@
-import { NextFunction } from "express";
+import { NextFunction, Request , Response } from "express";
 import {z } from "zod";
 import { AppointmentModel } from "../../models/appointmentsModel";
 import { Types } from "mongoose";
+import { DoctorModel } from "../../models/doctorModel";
 
 export const CancelAppointmentController = async (req:Request , res: Response , next:NextFunction) => { 
     const parsedDataWithSuccess = await z.object({
@@ -9,21 +10,43 @@ export const CancelAppointmentController = async (req:Request , res: Response , 
     }).safeParse(req.body)
 
     if(parsedDataWithSuccess.success){
-        const {appointmentId} = parsedDataWithSuccess.data
-        const appointmentToDelete = await AppointmentModel.findOne({_id: new Types.ObjectId(appointmentId)})
+        try{
+            const {appointmentId} = parsedDataWithSuccess.data
+            const appointmentToDelete = await AppointmentModel.findOne({_id: new Types.ObjectId(appointmentId)})
 
-        if(!appointmentToDelete)   {
-            res.status(404).json({
-            success:false , 
-            message: "Appointment not found"
-        })
-        return 
-        {}
-    
-        // return;
-        // const doctorId = appointmentToDelete?.doctorId
+            if(!appointmentToDelete)   {
+                res.status(404).json({
+                    success:false , 
+                    message: "Appointment not found"
+                })
+                return;
+            }
+        
+            await AppointmentModel.deleteOne({
+                _id: new Types.ObjectId(parsedDataWithSuccess.data.appointmentId)
+            })
+
+            const doctorId = appointmentToDelete.doctorId
+            await DoctorModel.updateOne(
+                {_id: doctorId},
+                {$unset:{[`slots_booked.${appointmentId}`]:""}}
+            )
+            res.status(200).json({
+                success: true ,
+                message: "Appointment cancelled successfully"
+            })
+        }catch(error){
+            res.status(500).json({
+                success: false ,
+                error
+            })
+        }
     }
     else{
-
+        res.status(400).json({
+            success:false ,
+            message: "incorrect format , try again"
+        })
     }
+
 }
