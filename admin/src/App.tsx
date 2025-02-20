2bd7c94b4b9155a2ad218483a327fec57dc91f5d
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-02-20 10:08:58 +0530

    set the frontend routes using BrowserRouter

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Navbar } from './components/Navbar'
+import { AdminDashboard } from './pages/adminDashboard'
+import { AppointmentList } from './pages/appointmentList'
+import { DoctorList } from './pages/doctorList'
+import { AddDoctor } from './pages/addDoctor'
 
 const App = () => {
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element:<Navbar/>,
+      children:[
+        {
+          path:"/admin-dashboard",
+          element:<AdminDashboard/>
+        },
+        {
+          path:"add-doctor",
+          element:<AddDoctor/>
+        },
+        {
+          path:"doctor-list",
+          element:<DoctorList/>
+        },
+        {
+          path:"appointment-list",
+          element:<AppointmentList/>
+        }
+      ]
+    }
+  ])
   return (
-    <div>App</div>
+    <div>
+      <RouterProvider router= {router}/>
+    </div>
   )
 }
 
---
+++ admin/src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <>
+        <div className="">
+            <div className="">
+                <Navlink to="/admin-dashboard"></Navlink>
+            </div>
+            <div className=""></div>
+        </div>
+    </>
+  )
+}
