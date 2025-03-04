60c9a9097172116354f1c02673e80aab02656abb
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-03-04 22:44:34 +0530

    added the functionality to convert an image to base64

---
+++ admin/src/utils/imageUpload.ts
@@ -0,0 +1,32 @@
+import { useState } from "react"
+
+
+const[postImage, setPostImage] = useState<any>(undefined)
+export const ImageUpload = async (e: React.ChangeEvent<HTMLInputElement>) => { 
+    const file= e?.target?.files?.[0]
+    
+    if (file) {
+        const base64 = await convertToBase64(file) 
+        setPostImage(base64)
+        
+    } else {
+       throw new Error("couldn't upload the image try again") 
+    }
+}
+
+export const convertToBase64 =  (file:any) => { 
+    return new Promise((resolve, reject) => { 
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(file);
+
+        fileReader.onload = () => { 
+            resolve(fileReader.result )
+         }
+        
+        fileReader.onerror = (error) => { 
+            reject(error)
+         }
+     })
+ }
+
+ export {postImage , setPostImage}
