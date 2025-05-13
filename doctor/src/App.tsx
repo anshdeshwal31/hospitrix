d47d784d3e468d6540eadd739bf9343b15a4740b
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-05-13 18:16:38 +0530

    set the paths for the pages in the App.tsx

--- doctor/src/App.tsx
+++ doctor/src/App.tsx
@@ -1,10 +1,43 @@
 import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Navbar } from './components/Navbar'
+import { AppointmentList } from './pages/AppointmentList'
+import { DoctorDashboard } from './pages/DoctorDashboard'
+import { DoctorLogin } from './pages/DoctorLogin'
+import { DoctorProfile } from './pages/DoctorProfile'
+import { ToastContainer } from 'react-toastify'
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path:"login",
+      element:<DoctorLogin/>
+    },
+    {
+    path:'/',
+    element:<Navbar/>,
+    errorElement:<div className='text-center font-semibold text-3xl'>Error loading page</div>,
+    children:[
+      {
+        path:'/doctor-dashboard',
+        element:<DoctorDashboard/>
+      },
+      {
+        path:'appointment-list',
+        element:<AppointmentList/>
+      },
+      {
+        path:'my-profile',
+        element:<DoctorProfile/>
+      }
+    ]
+  }])
+
 
   return (
-    <div className='bg-green-300'>
-      wassup lad
+    <div>
+      <RouterProvider router= {router}/>
+      <ToastContainer position="top-right" autoClose={3000} />
     </div>
   )
 }
