8d56350239b99dea75ee9352949401d521c3fc16
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-06-03 17:18:54 +0530

    made the my appointments page of the user panel responsive

--- frontend/src/pages/MyAppointments.tsx
+++ frontend/src/pages/MyAppointments.tsx
@@ -21,11 +21,11 @@ const MyAppointments:React.FC = () => {
   return (
 
       <div className='w-full flex justify-center'>
-        <div className=' w-[80%]'>
+        <div className='sm:p-0 pl-3 w-[100%] sm:w-[70%] lg:w-[65%] flex flex-col gap-6 '>
           <div className="self-start font-medium text-slate-600 text-xl mt-8">My appointments</div>
           <hr  className='my-2'/>
           {appointmentList && appointmentList.map((item:appointmentType,index:number)=>(
-            <div className='grid grid-cols-[1fr_2fr] gap-4 w-full sm:flex text-lg sm:gap-6 py-2 border-b' key={index}>
+            <div className='grid grid-cols-[1fr_2fr] gap-4 w-full sm:flex text-lg sm:gap-6 py-2 pb-4 border-b-[1px] border-slate-300' key={index}>
               <div>
                 <img className='w-48 bg-indigo-50 h-[210px] object-cover object-top' src={item.doctorId.image}  alt="" />
               </div>
@@ -40,14 +40,15 @@ const MyAppointments:React.FC = () => {
               </div>
               <div></div>
               {item.isPending?<div className='flex flex-col gap-2 justify-end mr-6 '>
-                <button className='bg-primary-blue font-light text-base p-2 text-white text-center sm:min-w-48 border rounded-md' onClick={() => { payOnline(item._id,item.doctorId.fees) }}>Pay Online</button>
-                <button className='bg-red-500 font-light text-base p-2 text-white text-center sm:min-w-48 border rounded-md' onClick={() => { cancelAppointment(item._id) }}>Cancel</button>
+                <button className='bg-primary-blue font-light text-base p-2 text-white text-center w-fit sm:px-0 px-14 sm:min-w-48 border rounded-md' onClick={() => { payOnline(item._id,item.doctorId.fees) }}>Pay Online</button>
+                <button className='bg-red-500 font-light text-base p-2 text-white w-fit sm:px-0 px-[70px] text-center sm:min-w-48 border rounded-md' onClick={() => { cancelAppointment(item._id) }}>Cancel</button>
               </div>:
-              <div className='self-end  mr-6'>{item.isCompleted?<button className=' font-light text-base p-2 text-green-500 border-green-500 text-center sm:min-w-48 border rounded-md'>Completed</button>:
-              <button className=' border-red-500 font-light text-base p-2 text-red-500 text-center sm:min-w-48 border rounded-md'>Cancelled</button>
+              <div className='self-end  mr-6'>{item.isCompleted?<button className=' font-light text-base p-2 text-green-500 border-green-500 text-center w-fit sm:px-0 px-14 sm:min-w-48 border rounded-md'>Completed</button>:
+              <button className=' border-red-500 font-light text-base p-2 text-red-500 text-center sm:min-w-48 border rounded-md w-fit sm:px-0 px-14'>Cancelled</button>
                 }
               </div>
               }
+              {/* <hr className='bg-black h-1 ml-40 w-[100px]' /> */}
             </div>
           ))}
         </div>
