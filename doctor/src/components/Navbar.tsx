d069268c581afe06b1a35c61c5e361ebd5ce3ccf
anshdeshwal31 <anshdeshwal0011@gmail.com>
2025-06-08 00:08:12 +0530

    made the navbar of the doctor panel responsive

--- doctor/src/components/Navbar.tsx
+++ doctor/src/components/Navbar.tsx
@@ -6,15 +6,15 @@ export const Navbar = () => {
   return (
     <div className="">
 
-        <div className="flex justify-between items-center px-6 mx-8 my-4">
+        <div className="flex  sm:justify-between items-center pl-6 mx-2 sm:mx-8 my-4">
 
             <div className="flex gap-3 items-center">
-                <NavLink to="/doctor-dashboard"><img src={assets.admin_logo} alt="" /></NavLink>
+                <NavLink to="/doctor-dashboard"><img src={assets.admin_logo} alt="" className="self-start" /></NavLink>
 
-                <div className="text-slate-600"><button className="border rounded-full px-2 ml-3 border-slate-400">Doctor</button></div>
+                <div className="text-slate-600"><button className="border rounded-full px-3 py-1 sm:ml-3 mr-3 border-slate-400 ">Doctor</button></div>
             </div>
             <div className="">
-                <button className=" bg-primary-blue rounded-full py-3 px-11 text-white text-lg hover:cursor-pointer">Logout</button>
+                <button className=" bg-primary-blue rounded-full py-2 px-5 sm:py-3 sm:px-11 text-white text-lg hover:cursor-pointer">Logout</button>
             </div>
 
         </div>
